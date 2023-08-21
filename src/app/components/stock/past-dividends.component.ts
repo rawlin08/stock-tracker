edaf5bb7f637c774bee9fdc1415db3d5bb4d068d
Alec Rawlins <alec12999@yahoo.com>
2023-08-21 01:28:22 -0400

    data input (beta)

--- src/app/components/stock/past-dividends.component.ts
+++ src/app/components/stock/past-dividends.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { StockComponent } from './stock.component';
 
 @Component({
   selector: 'app-past-dividends',
-  template: ``,
-  styles: [``]
+  template: `
+  <h3 class="dataNotFound" *ngIf="!this.stockComponent.stock.dividends[0]">No Dividend Data Found</h3>
+  <table *ngIf="this.stockComponent.stock.dividends[0]">
+    <tr>
+      <th>Ex-Div Date</th>
+      <th>Dividend Per Share</th>
+      <th>Frequency</th>
+    </tr>
+    <tr *ngFor="let element of this.stockComponent.stock.dividends">
+      <td>{{ element.ex_dividend_date }}</td>
+      <td>{{ round(element.cash_amount) }} {{ element.currency }}</td>
+      <td>{{ getFrequency(element.frequency) }}</td>
+    </tr>
+  </table>
+  `,
+  styles: [`
+  table {
+    width: 100%;
+    margin: 10px 0 0 0;
+  }
+  table > tr {
+    display: flex;
+    justify-content: space-between;
+    margin: 0 0 10px 0;
+  }
+  `]
 })
 export class PastDividendsComponent {
+  constructor(public stockComponent: StockComponent) {}
 
+  getFrequency(frequency:any) {
+    let freq;
+    switch(frequency) {
+      case 12:
+        freq = 'Monthly'
+        break
+      case 6:
+        freq = 'Bi-Yearly'
+        break
+      case 4:
+        freq = 'Quarterly'
+        break
+      case 1:
+        freq = 'Yearly'
+        break
+      case 0:
+        freq = 'Special'
+    }
+    return freq;
+  }
+  round(num: any) {
+    let split = num.toString().split('.');
+    if (!split[1] || split[1].length == 1 || split[1].length == 2 || split[1].length == 3) {
+      return num
+    }
+    else {
+      let n = Math.round(num * 10000) / 10000
+      let a = n.toFixed(4);
+      return a
+    }
+  }
 }
