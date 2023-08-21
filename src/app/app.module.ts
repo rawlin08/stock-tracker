584dc336669b0be26612c7cc7f76b56f14f77853
Alec Rawlins <alec12999@yahoo.com>
2023-08-21 01:27:48 -0400

    organizing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+// Components
 import { AppComponent } from './app.component';
 import { StockComponent } from './components/stock/stock.component';
-import { HttpClientModule } from '@angular/common/http';
 import { OverviewComponent } from './components/stock/overview.component';
 import { AboutComponent } from './components/stock/about.component';
 import { MRDividendComponent } from './components/stock/MRdividend.component';
 import { FinancialsComponent } from './components/stock/financials.component';
 import { SplitsComponent } from './components/stock/splits.component';
 import { PastDividendsComponent } from './components/stock/past-dividends.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTabsModule } from '@angular/material/tabs';
 import { CompanyComponent } from './components/stock/company.component';
 import { AboutStockComponent } from './components/stock/about-stock.component';
 import { NewsComponent } from './components/stock/news.component';
+import { PastComponent } from './components/stock/past.component';
+import { PastSplitsComponent } from './components/stock/past-splits.component';
+import { PastFinancialsComponent } from './components/stock/past-financials.component';
+
+// Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { PriceComponent } from './components/stock/price.component';
 
 @NgModule({
     declarations: [
@@ -29,7 +38,11 @@ import { NewsComponent } from './components/stock/news.component';
         PastDividendsComponent,
         CompanyComponent,
         AboutStockComponent,
-        NewsComponent
+        NewsComponent,
+        PastComponent,
+        PastSplitsComponent,
+        PastFinancialsComponent,
+        PriceComponent
     ],
     providers: [],
     bootstrap: [AppComponent],
@@ -38,7 +51,9 @@ import { NewsComponent } from './components/stock/news.component';
         HttpClientModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatTabsModule
+        MatTabsModule,
+        MatSortModule,
+        MatTableModule
     ]
 })
 export class AppModule { }
