1a4c0b019e6c0c09ab154c4d527d6840b93f35f5
Alec Rawlins <alec12999@yahoo.com>
2023-08-20 20:08:17 -0400

    boiler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StockComponent } from './components/stock/stock.component';
+import { HttpClientModule } from '@angular/common/http';
+import { OverviewComponent } from './components/stock/overview.component';
+import { AboutComponent } from './components/stock/about.component';
+import { MRDividendComponent } from './components/stock/MRdividend.component';
+import { FinancialsComponent } from './components/stock/financials.component';
+import { SplitsComponent } from './components/stock/splits.component';
+import { PastDividendsComponent } from './components/stock/past-dividends.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CompanyComponent } from './components/stock/company.component';
+import { AboutStockComponent } from './components/stock/about-stock.component';
+import { NewsComponent } from './components/stock/news.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        StockComponent,
+        OverviewComponent,
+        AboutComponent,
+        MRDividendComponent,
+        FinancialsComponent,
+        SplitsComponent,
+        PastDividendsComponent,
+        CompanyComponent,
+        AboutStockComponent,
+        NewsComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatTabsModule
+    ]
 })
 export class AppModule { }
