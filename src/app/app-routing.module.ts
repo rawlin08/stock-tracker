d94850092381db097d39201f4cac9780c939001f
Alec Rawlins <alec12999@yahoo.com>
2023-09-19 17:45:19 -0400

    testing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StockSearchComponent } from './components/stock/search.component';
+import { StockComponent } from './components/stock/stock.component';
+import { OverviewComponent } from './components/stock/overview.component';
+import { NewsComponent } from './components/stock/news.component';
+import { CompanyComponent } from './components/stock/company.component';
+import { PastComponent } from './components/stock/past.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', title: 'Stock Search', component: StockSearchComponent },
+  { path: ':stock', component: StockComponent, },
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
