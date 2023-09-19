a64d92325809a9ac8b3e11f7245f6fbfdffba942
Alec Rawlins <alec12999@yahoo.com>
2023-09-19 17:46:47 -0400

    activated router, search ticker based on URL

--- src/app/components/stock/stock.component.ts
+++ src/app/components/stock/stock.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { StockAPIService } from 'src/app/services/stock-api.service';
 
 @Component({
@@ -10,59 +11,62 @@ import { StockAPIService } from 'src/app/services/stock-api.service';
     <mat-tab label="News"><app-news></app-news></mat-tab>
     <mat-tab label="Company"><app-company></app-company></mat-tab>
   </mat-tab-group>
-
-  <app-past></app-past>
   `,
   styles: [``]
 })
 export class StockComponent implements OnInit {
-  constructor(public stockapi: StockAPIService) {}
+  constructor(public stockapi: StockAPIService, public route: ActivatedRoute, public router: Router){}
   ngOnInit() {
-    this.getStockInfo();
 
-    // Connecting to stock websocket to get 15 min delayed data streamed to the site
-    this.stocksWS = new WebSocket('wss://delayed.polygon.io/stocks')
-    this.stocksWS.onopen = () => {
-      this.stocksWS.send(`{"action":"auth","params":"${this.APIKEY}"}`) // Authorize
-      this.stocksWS.send(`{"action":"subscribe", "params":"A.${this.TESTSTOCK}"}`); // Which stock to subscribe to
-    }
-    this.stocksWS.onmessage = (data:any) => {
-
-      // Grab data coming in and parse the stringed object
-      let message = data;
-      message = JSON.parse(message.data)
-      console.log(message);
-
-      // if the message being recieved is stock data, round the price to 2 decimal places and display on screen
-      if (message[0].ev == "A") {
-        let unroundedPrice = message[0].o
-        console.log(unroundedPrice);
-        let n = Math.round(unroundedPrice * 100) / 100
-        this.price = n.toFixed(2);
-      }
-      else { // Message recieved was a connected, subscribed, or authorized completed message
-        this.price = "--"
-        let today = new Date().toISOString().slice(0, 10)
-        console.log(today)
-        this.stockapi.getTickerLastTrade(this.TESTSTOCK, '2023-08-18').subscribe((data) => {
-          console.log(data);
-          this.lastTrade = data;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let uid: any = params.get('stock');
+      this.getStockInfo(uid);
+      let today = new Date().toISOString().slice(0, 10)
+      console.log(today)
+      this.stockapi.getTickerLastTrade(uid, today).subscribe(
+        res => {
+          console.log(res);
+          
+          this.lastTrade = res;
           this.lastTrade = this.lastTrade.results[0].o
           let n = Math.round(this.lastTrade * 100) / 100
           this.price = n.toFixed(2);
-        });
-      }
-    };
 
-    
+          // Connecting to stock websocket to get 15 min delayed data streamed to the site
+          this.stocksWS = new WebSocket('wss://delayed.polygon.io/stocks')
+          this.stocksWS.onopen = () => {
+            this.stocksWS.send(`{"action":"auth","params":"${this.APIKEY}"}`) // Authorize
+            this.stocksWS.send(`{"action":"subscribe", "params":"A.${uid}"}`); // Which stock to subscribe to
+          }
+          this.stocksWS.onmessage = (data:any) => {
 
+            // Grab data coming in and parse the stringed object
+            let message = data;
+            message = JSON.parse(message.data)
+            console.log(message);
+
+            // if the message being recieved is stock data, round the price to 2 decimal places and display on screen
+            if (message[0].ev == "A") {
+              let unroundedPrice = message[0].o
+              console.log(unroundedPrice);
+              let n = Math.round(unroundedPrice * 100) / 100
+              this.price = n.toFixed(2);
+            }
+            else { // Message recieved was a connected, subscribed, or authorized completed message
+              
+            }
+          };
+        },
+        err => {
+          console.log('HTTP Error', err);
+        }
+      );
+    });
   }
   APIKEY:any = 'TKVSXdx635Dera7_JxMwbX3fQBc1Q77t';
   stocksWS:any = {}
   price:any;
   lastTrade:any;
-  
-  TESTSTOCK: string = 'SVOL';
 
   stock:any = {
     details: {},
@@ -72,29 +76,29 @@ export class StockComponent implements OnInit {
     news: [],
     snapshot: {}
   };
-  getStockInfo() {
-    this.stockapi.getTickerDetails(this.TESTSTOCK).subscribe((data) => {
+  getStockInfo(stock:any) {
+    this.stockapi.getTickerDetails(stock).subscribe((data) => {
       this.stock.details = data;
       this.stock.details = this.stock.details.results;
     });
-    this.stockapi.getTickerDividends(this.TESTSTOCK).subscribe((data) => {
+    this.stockapi.getTickerDividends(stock).subscribe((data) => {
       this.stock.dividends = data;
       this.stock.dividends = this.stock.dividends.results;
     });
-    this.stockapi.getTickerSplits(this.TESTSTOCK).subscribe((data) => {
+    this.stockapi.getTickerSplits(stock).subscribe((data) => {
       this.stock.splits = data;
       this.stock.splits = this.stock.splits.results;
     });
-    this.stockapi.getTickerFinancials(this.TESTSTOCK).subscribe((data) => {
+    this.stockapi.getTickerFinancials(stock).subscribe((data) => {
       this.stock.financials = data;
       this.stock.financials = this.stock.financials.results;
     });
-    this.stockapi.getTickerNews(this.TESTSTOCK).subscribe((data) => {
+    this.stockapi.getTickerNews(stock).subscribe((data) => {
       this.stock.news = data;
       this.stock.news = this.stock.news.results;
     });
     console.log(this.stock);
-    this.stockapi.getOverallStockData(this.TESTSTOCK).subscribe((data) => {
+    this.stockapi.getOverallStockData(stock).subscribe((data) => {
       this.stock.snapshot = data;
       this.stock.snapshot = this.stock.snapshot.ticker;
     });
