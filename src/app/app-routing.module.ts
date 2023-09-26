9ffe448ab619c678be155282bf145e3565c09868
Alec Rawlins <alec12999@yahoo.com>
2023-09-26 16:39:49 -0400

    route for full screen company details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { StockSearchComponent } from './components/stock/search.component';
 import { StockComponent } from './components/stock/stock.component';
 import { OverviewComponent } from './components/stock/overview.component';
 import { NewsComponent } from './components/stock/news.component';
-import { CompanyComponent } from './components/stock/company.component';
-import { PastComponent } from './components/stock/past.component';
+import { CompanyComponent } from './components/stock/company/company.component';
+import { PastComponent } from './components/stock/company/past/past.component';
+import { CompanyDetailsComponent } from './components/stock/company/company-details.component';
 
 const routes: Routes = [
   { path: 'stock', title: 'Stock Search', component: StockSearchComponent },
   { path: 'stock/:stock', component: StockComponent },
   { path: 'stock/:stock/past', component: PastComponent },
-  
+  { path: 'stock/:stock/company-details', component: CompanyDetailsComponent }
 ];
 
 @NgModule({
